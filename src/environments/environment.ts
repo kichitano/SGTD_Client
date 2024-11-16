1f66c2172aac2ea5fce9b5cb29afc9d412ca1ef1
kichitano <30935429+kichitano@users.noreply.github.com>
2024-11-15 23:38:04 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:44319/api'
+    apiUrl: 'https://sgtd-webapi-hbeee6f2d3ffcmhh.eastus2-01.azurewebsites.net/api'
 };
